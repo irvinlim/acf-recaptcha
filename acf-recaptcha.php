9a4b53e6c0c7dc137b798abbfd07c73453b4533e
UnaiYecora <unaiyecora@gmail.com>
2016-01-27 18:21:50 +0000

    Fix plugin info

--- acf-recaptcha.php
+++ acf-recaptcha.php
@@ -1,47 +1,51 @@
 <?php
 
 /*
-Plugin Name: Advanced Custom Fields: reCAPTCHA Field
-Plugin URI: https://github.com/irvinlim/acf-recaptcha/
-Description: Google reCAPTCHA Field for Advanced Custom Fields. See <a href="https://www.google.com/recaptcha/">https://www.google.com/recaptcha/</a> for an account.
-Version: 1.0.2
-Author: Irvin Lim
-Author URI: http://irvinlim.com
+Plugin Name: ACF reCAPTCHA
+Plugin URI: https://github.com/UnaiYecora/acf-recaptcha
+Description: Reduce spam with reCAPTCHA on your Advanced Custom Fields frontend forms. Currently only supports ACF v5.
+Version: 1.2
+Author: Unai Yécora
+Author URI: http://unaiyecora.com
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Original Plugin Name: Advanced Custom Fields: reCAPTCHA Field
+Original Author: Irvin Lim
+Original Author URI: http://irvinlim.com
+Original Plugin URI: https://github.com/irvinlim/acf-recaptcha/
 */
 
 
 
 
-load_plugin_textdomain( 'acf-recaptcha', false, dirname( plugin_basename(__FILE__) ) . '/lang/' ); 
+load_plugin_textdomain( 'acf-recaptcha', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
 
 
 
 function include_field_types_recaptcha( $version ) {
-	
+
 	include_once('acf-recaptcha-v5.php');
-	
+
 }
 
-add_action('acf/include_field_types', 'include_field_types_recaptcha');	
+add_action('acf/include_field_types', 'include_field_types_recaptcha');
 
 
 
 
-/* 
+/*
  * ACF 4 support coming soon!
  */
 
 /*function register_fields_recaptcha() {
-	
+
 	include_once('acf-recaptcha-v4.php');
-	
+
 }
 
 add_action('acf/register_fields', 'register_fields_recaptcha');	*/
 
 
 
-	
+
 ?>
