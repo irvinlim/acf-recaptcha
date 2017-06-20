1e8d580a217635bace434df17ec3697de789271f
Irvin Lim <irvinlimweiquan@gmail.com>
2017-06-21 02:22:42 +0800

    Update version in acf-recaptcha.php

--- acf-recaptcha.php
+++ acf-recaptcha.php
@@ -4,7 +4,7 @@
 Plugin Name: Advanced Custom Fields: reCAPTCHA Field
 Plugin URI: https://github.com/irvinlim/acf-recaptcha/
 Description: Google reCAPTCHA Field for Advanced Custom Fields. See <a href="https://www.google.com/recaptcha/">https://www.google.com/recaptcha/</a> for an account.
-Version: 1.0.8
+Version: 1.1.0
 Author: Irvin Lim
 Author URI: https://irvinlim.com
 License: MIT
@@ -15,10 +15,10 @@ License URI: https://opensource.org/licenses/MIT
 load_plugin_textdomain('acf-recaptcha', false, dirname(plugin_basename(__FILE__)) . '/lang/');
 
 
-function include_field_types_recaptcha($version) {
+function include_field_types_recaptcha($version)
+{
 
     include_once('acf-recaptcha-v5.php');
-
 }
 
 add_action('acf/include_field_types', 'include_field_types_recaptcha');
