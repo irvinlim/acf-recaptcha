219b1e00b8b1ce587ad89d5cef3caf8acb937f53
Irvin Lim <irvinlimweiquan@gmail.com>
2017-07-03 02:23:58 +0800

    Fix formatting

--- acf-recaptcha.php
+++ acf-recaptcha.php
@@ -13,19 +13,27 @@ License URI: https://opensource.org/licenses/MIT
 
 global $pagenow;
 
-// Loads the text domain.
+/** 
+ * Loads the text domain.
+ */
 load_plugin_textdomain('acf-recaptcha', false, dirname(plugin_basename(__FILE__)) . '/lang/');
 
-// Loads the acf_field classes.
+
+/** 
+ * Loads the acf_field classes.
+ */
 function include_field_types_recaptcha($version) {
     include_once('acf-recaptcha-v5.php');
 }
 
 add_action('acf/include_field_types', 'include_field_types_recaptcha');
 
+
+/** 
+ * Adds the WordPress plugin update action if we are on the plugins page.
+ */
 include_once('includes/plugin-update.php');
 
-// Adds the WordPress plugin update action if we are on the plugins page.
 if ('plugins.php' === $pagenow) {
     $file = basename(__FILE__);
     $folder = basename(dirname(__FILE__));
