f4cd854e64b59ee1702a5e3a9e1ce7266aff312a
Irvin Lim <irvinlimweiquan@gmail.com>
2017-07-03 04:43:22 +0800

    Fix typo and use span for upgrade notice block (already nested in p tag)

--- includes/plugin-update.php
+++ includes/plugin-update.php
@@ -13,14 +13,14 @@ global $pagenow;
  * @param array $plugin_data    Array of plugin data.
  * @param array $r              Array of metadata about the plugin update.
  */
-function in_plugin_update_message($plugin_data, $r) { 
+function in_plugin_update_message_acf_recaptcha($plugin_data, $r) { 
     if (isset($r->upgrade_notice) && strlen(trim($r->upgrade_notice)) > 0) {
         ?>
 
-        <p style="background-color: #d54e21; padding: 10px; color: #f9f9f9; margin-top: 10px">
+        <span style="display: block; background-color: #d54e21; padding: 10px; color: #f9f9f9; margin: 10px 0">
             <strong>Upgrade Notice:</strong>
             <?php echo esc_html($r->upgrade_notice); ?>
-        </p>
+        </span>
 
         <?php
     }
